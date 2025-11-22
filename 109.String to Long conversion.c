0243ab817ce72e271911d305804ebecd09a85c32
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-23 03:23:40 +0600

    109.String to Long conversion.c

---
+++ 109.String to Long conversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+int main() {
+   char str[] = "12345678";
+   long num;
+
+         num = atol(str);   
+
+ printf("Long = %ld\n", num);
+    return 0;
+}
