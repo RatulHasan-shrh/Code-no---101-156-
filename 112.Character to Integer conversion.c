0ee115a2af4417eba579ed2c92b6dfe4475bc3d5
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-23 03:29:50 +0600

    112.Character to Integer conversion.c

---
+++ 112.Character to Integer conversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+          printf("Enter a character: ");
+          scanf("%c", &ch);
+
+ printf("ASCII value = %d\n", (int)ch);
+
+    return 0;
+}
