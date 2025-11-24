28e02c8ee1f94e80234fc85d35c75f625f3da52c
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-24 09:49:50 +0600

    120.Two Dimensional Array of Two Pointers.c

---
+++ 120.Two Dimensional Array of Two Pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30, d = 40;
+  
+
+        int *arr[2][2] = {
+           { &a, &b },
+           { &c, &d }
+};
+
+   printf("%d\n", *arr[0][1]); 
+      return 0;
+}
